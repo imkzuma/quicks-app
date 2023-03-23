54f1e01aa3ec0669a599899f6b5c758f6aaf7976
imkzuma <edu.gungkrisna@gmail.com>
2023-03-24 01:18:30 +0800

    [ADD] Addedd Icon Button when Children Null

--- components/Quicks/ToggleTasksView.js
+++ components/Quicks/ToggleTasksView.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { Button, useAccordionButton } from "react-bootstrap"
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 export function ToggleTasksView({ children, eventKey }){
-    const decoratedOnClick = useAccordionButton(eventKey, () =>
-        console.log('totally custom!')
-    );
+    const decoratedOnClick = useAccordionButton(eventKey);
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+        decoratedOnClick();
+    }
 
     return(
-        <Button type = "button" onClick = { decoratedOnClick }> { children } </Button>
+        <Button variant = "transparent" type = "button" onClick = { handleClick }> 
+            {
+                children ? children : open ? <BsChevronDown /> : <BsChevronUp /> 
+            }
+        </Button>
     )
 }
