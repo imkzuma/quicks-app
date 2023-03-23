c54b8408c45fdc79d0b0a07d9d9685b507ce4533
imkzuma <edu.gungkrisna@gmail.com>
2023-03-24 00:52:33 +0800

    [FIXED] Fixed Bug In Quicks Button

--- components/Quicks/QuicksButton.js
+++ components/Quicks/QuicksButton.js
@@ -22,6 +22,58 @@ export default function QuicksButton(){
         setShowTodo(false);
     }
 
+    function BtnTodo() {
+        return(
+            <Stack direction = "vertical" gap = {2}>
+                { !showChat&& !showTodo&& 
+                    <small className = 'text-white text-center fw-bold'>Tasks</small>
+                }
+                <Button 
+                    onClick = { handleTodoClick } 
+                    className = 'rounded-circle p-3'
+                    style = {{
+                        background: showTodo? "#F8B76B" : "#F2F2F2", 
+                        border: `1px solid ${showTodo? "#F8B76B" : "#f2f2f2"}`
+                    }}
+                >
+                    <Image 
+                        src = {`assets/icon/${showTodo? "todo-white" : "todo-color"}.svg`}
+                        width = "26"
+                        height = "20"
+                        priority
+                        alt = "Todo Icon"
+                    />
+                </Button>
+            </Stack>
+        )
+    }
+
+    function BtnChat(){
+        return(
+            <Stack direction = "vertical" gap = {2}>
+                { !showChat&& !showTodo&& 
+                    <small className = 'text-white text-center fw-bold'>Inbox</small>
+                }
+                <Button 
+                    onClick = { handleChatClick } 
+                    className = 'rounded-circle p-3 quicks-btn-chat' 
+                    style = {{
+                        background: showChat ? "#8785FF" : "#F2F2F2", 
+                        border: `1px solid ${showChat? "#8785FF" : "#f2f2f2"}`
+                    }}
+                >
+                    <Image 
+                        src = {`assets/icon/${showChat? "chat-white" :"chat-color"}.svg`}
+                        width = "26"
+                        height = "20"
+                        priority
+                        alt = "Chat Icon"
+                    />
+                </Button>
+            </Stack>
+        )
+    }
+
     return(
         <>
             <AnimatePresence>
@@ -69,47 +121,21 @@ export default function QuicksButton(){
                                 exit = {{ opacity: 0, x: 50 }}
                                 className = 'd-flex gap-3 overflow-hidden'
                             >
-                                <Stack direction = "vertical" gap = {2}>
-                                    { !showChat&& !showTodo&& 
-                                        <small className = 'text-white text-center fw-bold'>Tasks</small>
-                                    }
-                                    <Button 
-                                        onClick = { handleTodoClick } 
-                                        className = 'rounded-circle p-3'
-                                        style = {{
-                                            background: showTodo? "#F8B76B" : "#F2F2F2", 
-                                            border: `1px solid ${showTodo? "#F8B76B" : "#f2f2f2"}`
-                                        }}
-                                    >
-                                        <Image 
-                                            src = {`assets/icon/${showTodo? "todo-white" : "todo-color"}.svg`}
-                                            width = "26"
-                                            height = "20"
-                                            alt = "Todo Icon"
-                                        />
-                                    </Button>
-                                </Stack>
-
-                                <Stack direction = "vertical" gap = {2}>
-                                    { !showChat&& !showTodo&& 
-                                        <small className = 'text-white text-center fw-bold'>Inbox</small>
-                                    }
-                                    <Button 
-                                        onClick = { handleChatClick } 
-                                        className = 'rounded-circle p-3 quicks-btn-chat' 
-                                        style = {{
-                                            background: showChat ? "#8785FF" : "#F2F2F2", 
-                                            border: `1px solid ${showChat? "#8785FF" : "#f2f2f2"}`
-                                        }}
-                                    >
-                                        <Image 
-                                            src = {`assets/icon/${showChat? "chat-white" :"chat-color"}.svg`}
-                                            width = "26"
-                                            height = "20"
-                                            alt = "Chat Icon"
-                                        />
-                                    </Button>
-                                </Stack>
+                                <>
+                                    {(!showChat && !showTodo) && <>
+                                        <BtnTodo />
+                                        <BtnChat />
+                                    </>}
+                                    {(showChat && !showTodo) && <>
+                                        <BtnTodo />
+                                        <BtnChat />
+                                    </>}
+                                    {(showTodo && !showChat) && <>
+                                        <BtnChat />
+                                        <BtnTodo />
+                                    </>}
+                                </>
+                                
                             </motion.div>
                     }
                 </AnimatePresence>
@@ -121,6 +147,7 @@ export default function QuicksButton(){
                             src = "/assets/icon/quicks-icon.svg"
                             width = "24"
                             height = "18"
+                            priority
                             alt = "Quicks Icon"
                         />
                     </Button>
