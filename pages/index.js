146ea114be029a270316f9c1db426e085bc3d04e
imkzuma <edu.gungkrisna@gmail.com>
2023-03-25 01:30:21 +0800

    [ADDED] Addedd Title in Index

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,25 @@
 import MainLayout from '@/layouts/MainLayout';
+import Head from 'next/head';
 import { Container } from 'react-bootstrap';
 
 export default function Index(){
-  // const getData = async() => {
-  //   const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-  //   console.log(response);
-  // }
-
-  // const getTodos = async() => {
-  //   const response = await axios.get("https://jsonplaceholder.typicode.com/todos?userId=10");
-  //   console.log(response)
-  // }
-
-  // useEffect(() => {
-  //   getData();
-  //   getTodos();
-  // }, []);
-
   return(
-    <MainLayout>
-      <Container 
-        fluid
-        className = 'd-flex justify-content-center align-items-center bg-dark'
-        style = {{
-          minHeight: "100vh",
-        }}
-      >
+    <>
+      <Head>
+        <title> Quicks App </title>
+      </Head>
 
-      </Container>
-    </MainLayout>
+      <MainLayout>
+        <Container 
+          fluid
+          className = 'd-flex justify-content-center align-items-center bg-dark'
+          style = {{
+            minHeight: "100vh",
+          }}
+        >
+
+        </Container>
+      </MainLayout>
+    </>
   )
 }
